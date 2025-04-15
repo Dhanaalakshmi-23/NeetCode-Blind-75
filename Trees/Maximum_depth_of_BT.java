1ae90cd83b10e65e37b4fba8d55c5e1665fcf905
Dhanaa <dhanaalakshminarayanan@gmail.com>
2025-04-15 11:00:21 +0530

    NeetCode Solutions

---
+++ Trees/Maximum_depth_of_BT.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.right),maxDepth(root.left));
+    }
+}
